63e08137888da7d0528882f5ab23783d3a60fd89
Ivan Kostarev <iskostarev@ya.ru>
2022-12-01 17:37:59 +0300

    day01: combine solutions

--- day01/main.go
+++ day01/main.go
@@ -8,18 +8,76 @@ import (
 	"strconv"
 )
 
+type Selector struct {
+	size int
+	topvals []uint64 // sorted from min to max
+}
+
+func (selector *Selector) findPivot(val uint64) int {
+	for i := selector.size-1; i >= 0; i-- {
+		if selector.topvals[i] < val {
+			return i
+		}
+	}
+
+	panic("no pivot was found")
+}
+
+func (selector *Selector) Init(size int) {
+	selector.size = size
+	selector.topvals = make([]uint64, size)
+}
+
+func (selector *Selector) Insert(val uint64) {
+	if selector.topvals[0] >= val {
+		return
+	}
+
+	pivot := selector.findPivot(val)
+
+	for i := 0; i < pivot; i++ {
+		selector.topvals[i] = selector.topvals[i+1]
+	}
+	selector.topvals[pivot] = val
+}
+
+func (selector *Selector) Select() (result uint64) {
+	for _, val := range(selector.topvals) {
+		result += val
+	}
+	return
+}
+
+func (selector *Selector) DebugPrint() () {
+	line := ""
+	for _, val := range(selector.topvals) {
+		line += fmt.Sprintf("%d;", val)
+	}
+	fmt.Println(line)
+}
+
 func main() {
+	size := 1
+
+	if len(os.Args) > 1 {
+		var err error
+		size, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var max uint64 = 0
+	var selector Selector
+	selector.Init(size)
+
 	var cur uint64 = 0
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		if line == "" {
-			if cur > max {
-				max = cur
-			}
+			selector.Insert(cur)
 			cur = 0
 		} else {
 			calories, err := strconv.Atoi(line)
@@ -30,9 +88,6 @@ func main() {
 		}
 	}
 
-	if cur > max {
-		max = cur
-	}
-
-	fmt.Println(max)
+	selector.Insert(cur)
+	fmt.Println(selector.Select())
 }
--- day01_2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/iskostarev/aoc2022_day01_2
-
-go 1.19
