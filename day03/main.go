f82d3dc8754890cf74751d56fdf284954a9be9f6
Ivan Kostarev <iskostarev@ya.ru>
2022-12-03 16:27:01 +0300

    day03 part 2

--- day03/main.go
+++ day03/main.go
@@ -65,7 +65,44 @@ func (rucksack *Rucksack) findCommonItem() (result RucksackItem) {
 	panic("No common item")
 }
 
-func main() {
+func (rucksack *Rucksack) contains(item RucksackItem) bool {
+	for _, compartment := range [][]RucksackItem{rucksack.First, rucksack.Second} {
+		_, found := slices.BinarySearch(compartment, item)
+		if found {
+			return true
+		}
+	}
+	return false
+}
+
+func findBadge(group []Rucksack) (result RucksackItem) {
+	resultSet := false
+	checkCompartment := func(compartment []RucksackItem) {
+		for _, item := range compartment {
+			common := true
+			for i := 1; i < len(group); i++ {
+				if !group[i].contains(item) {
+					common = false
+					break
+				}
+			}
+
+			if common {
+				if resultSet && item != result {
+					panic(fmt.Sprintf("More then one common item type: %d, %d", result, item))
+				}
+				result = item
+				resultSet = true
+			}
+		}
+	}
+
+	checkCompartment(group[0].First)
+	checkCompartment(group[0].Second)
+	return
+}
+
+func mode1() {
 	scanner := bufio.NewScanner(os.Stdin)
 	priosum := 0
 	for scanner.Scan() {
@@ -74,4 +111,37 @@ func main() {
 		priosum += rucksack.findCommonItem().priority()
 	}
 	fmt.Println(priosum)
+}
+
+func mode2() {
+	const GROUP_SIZE = 3
+
+	scanner := bufio.NewScanner(os.Stdin)
+	priosum := 0
+	group := make([]Rucksack, 0, GROUP_SIZE)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		group = append(group, parseRucksack(line))
+		if len(group) != GROUP_SIZE {
+			continue
+		}
+
+		priosum += findBadge(group).priority()
+		group = group[:0]
+	}
+
+	if len(group) != 0 {
+		panic("Excess lines")
+	}
+
+	fmt.Println(priosum)
+}
+
+func main() {
+	if (len(os.Args) > 1) && (os.Args[1] == "2") {
+		mode2()
+	} else {
+		mode1()
+	}
 }
