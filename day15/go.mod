85b1ec751cd1f12a1e9296121c7104454de5f817
Ivan Kostarev <iskostarev@ya.ru>
2022-12-16 05:00:08 +0300

    day15 part 2

--- day15/go.mod
+++ day15/go.mod
@@ -1,3 +1,5 @@
 module github.com/iskostarev/aoc2022/day15
 
 go 1.19
+
+require golang.org/x/exp v0.0.0-20221215174704-0915cd710c24 // indirect
--- day15/main.go
+++ day15/main.go
@@ -7,6 +7,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 type Pos struct {
@@ -21,6 +23,15 @@ const (
 	CellBeacon
 )
 
+type LineChunk struct {
+	Min, Max int
+}
+
+type SparseLine struct {
+	min, max int
+	removed  []LineChunk
+}
+
 type Grid struct {
 	sensors map[Pos]int
 	beacons map[Pos]bool
@@ -28,6 +39,103 @@ type Grid struct {
 
 type Parser struct {
 	regex *regexp.Regexp
+}
+
+func MakeSparseLine(min, max int) (result SparseLine) {
+	result.min = min
+	result.max = max
+	result.removed = []LineChunk{}
+	return
+}
+
+func (sl *SparseLine) simplify() {
+	done := false
+	for !done {
+		done = true
+		newRemoved := []LineChunk{}
+		for _, chunk := range sl.removed {
+			if chunk.Max < sl.min || chunk.Min > sl.max {
+				continue
+			}
+
+			if chunk.Min <= sl.min && chunk.Max > sl.min {
+				sl.min = chunk.Max
+				done = false
+			}
+			if chunk.Max >= sl.max && chunk.Min < sl.max {
+				sl.max = chunk.Min
+				done = false
+			}
+
+			newRemoved = append(newRemoved, chunk)
+		}
+		sl.removed = newRemoved
+	}
+}
+
+func IntersectsOrAdjacent(lhs, rhs LineChunk) bool {
+	if lhs.Min > rhs.Min {
+		lhs, rhs = rhs, lhs
+	}
+	return lhs.Max >= rhs.Min
+}
+
+func Intersection(lhs, rhs LineChunk) LineChunk {
+	if lhs.Min > rhs.Min {
+		lhs, rhs = rhs, lhs
+	}
+	lhs.Min = rhs.Min
+	if rhs.Max < lhs.Max {
+		lhs.Max = rhs.Max
+	}
+	return lhs
+}
+
+func Union(lhs, rhs LineChunk) LineChunk {
+	if lhs.Min > rhs.Min {
+		lhs, rhs = rhs, lhs
+	}
+	if rhs.Max > lhs.Max {
+		lhs.Max = rhs.Max
+	}
+	return lhs
+}
+
+func (sl *SparseLine) RemoveChunk(chunk LineChunk) {
+	if chunk.Max < sl.min || chunk.Min > sl.max {
+		return
+	}
+
+	added := false
+	for i, hole := range sl.removed {
+		if IntersectsOrAdjacent(chunk, hole) {
+			sl.removed[i] = Union(chunk, hole)
+			added = true
+			break
+		}
+	}
+	if !added {
+		sl.removed = append(sl.removed, chunk)
+	}
+	sl.simplify()
+	slices.SortFunc(sl.removed, func(lhs, rhs LineChunk) bool {
+		return lhs.Min < rhs.Min
+	})
+}
+
+func (sl *SparseLine) Traverse(cb func(int)) {
+	for i := sl.min; i <= sl.max; i++ {
+		ok := true
+		for _, chunk := range sl.removed {
+			if chunk.Min <= i && i <= chunk.Max {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			cb(i)
+		}
+	}
 }
 
 func MakeGrid() (result Grid) {
@@ -57,6 +165,18 @@ func (grid *Grid) At(pos Pos) Cell {
 	}
 }
 
+func (grid *Grid) Reachable(pos Pos) bool {
+	if grid.At(pos) != CellEmpty {
+		return true
+	}
+	for sensor, radius := range grid.sensors {
+		if Distance(sensor, pos) <= radius {
+			return true
+		}
+	}
+	return false
+}
+
 func (grid *Grid) AddSensor(sensor, beacon Pos) {
 	grid.sensors[sensor] = Distance(sensor, beacon)
 	grid.beacons[beacon] = true
@@ -78,6 +198,92 @@ func (grid *Grid) NoBeaconsInRow(y int) int {
 		}
 	}
 	return len(marked)
+}
+
+func (grid *Grid) findDistressBeaconInRow(y, maxCoord int) (found bool, result int) {
+	candidates := MakeSparseLine(0, maxCoord)
+	for sensor, radius := range grid.sensors {
+		min, max := SensorRow(sensor, radius, y)
+		candidates.RemoveChunk(LineChunk{min, max})
+	}
+
+	found = false
+	candidates.Traverse(func(x int) {
+		if found {
+			panic("More than 1 beacon found")
+		}
+		found = true
+		result = x
+	})
+
+	return
+}
+
+func splitIntoChunks(min, max, chunks int) (result []LineChunk) {
+	result = []LineChunk{}
+	size := max - min + 1
+	if size%chunks != 0 {
+		size += (chunks - size%chunks)
+	}
+	length := size / chunks
+	curMin := min
+	for {
+		curMax := curMin + length - 1
+		if curMax > max {
+			curMax = max
+		}
+		result = append(result, LineChunk{curMin, curMax})
+		curMin += length
+		if curMin > max {
+			return
+		}
+	}
+}
+
+func (grid *Grid) FindDistressBeacon(maxCoord int) (result Pos) {
+	const jobCount = 16
+	const progressInterval = 100
+	segments := splitIntoChunks(0, maxCoord, jobCount)
+	resultChan := make(chan Pos)
+	done := make(chan int)
+	progressReports := make(chan int)
+	remainingJobs := len(segments)
+	progress := 0
+
+	for _, segment := range segments {
+		go func(segment LineChunk) {
+			lastProgress := segment.Min
+			for y := segment.Min; y <= segment.Max; y++ {
+				if y%progressInterval == 0 {
+					progressReports <- (y - lastProgress)
+					lastProgress = y
+				}
+				found, x := grid.findDistressBeaconInRow(y, maxCoord)
+				if found {
+					resultChan <- Pos{x, y}
+					break
+				}
+			}
+			progressReports <- (segment.Max - lastProgress)
+			done <- 0
+		}(segment)
+	}
+
+	for {
+		select {
+		case result = <-resultChan:
+			return
+		case progrep := <-progressReports:
+			progress += progrep
+			//fmt.Fprintf(os.Stderr, "Progress: %d/%d (%d%%)\n", progress, maxCoord, progress*100/maxCoord)
+		case <-done:
+			remainingJobs--
+			if remainingJobs == 0 {
+				panic("Beacon not found!")
+			}
+		}
+	}
+
 }
 
 func MakeParser() (result Parser) {
@@ -104,12 +310,6 @@ func (parser *Parser) ParseLine(line string) (sensor, beacon Pos) {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("Argument expected")
-	}
-
-	row := ParseInt(os.Args[1])
-
 	scanner := bufio.NewScanner(os.Stdin)
 	parser := MakeParser()
 	grid := MakeGrid()
@@ -120,5 +320,18 @@ func main() {
 		grid.AddSensor(sensor, beacon)
 	}
 
-	fmt.Println(grid.NoBeaconsInRow(row))
+	if len(os.Args) < 3 {
+		panic("Arguments expected")
+	}
+
+	if os.Args[1] == "1" {
+		row := ParseInt(os.Args[2])
+		fmt.Println(grid.NoBeaconsInRow(row))
+	} else if os.Args[1] == "2" {
+		maxCoord := ParseInt(os.Args[2])
+		pos := grid.FindDistressBeacon(maxCoord)
+		fmt.Println(pos.X*4000000 + pos.Y)
+	} else {
+		panic("First argument must be 1 or 2")
+	}
 }
