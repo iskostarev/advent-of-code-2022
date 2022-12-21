d9c4f5623699b8caf00667a8f61574f8d5a9e972
Ivan Kostarev <iskostarev@ya.ru>
2022-12-21 14:05:04 +0300

    day21 part 2

--- day21/main.go
+++ day21/main.go
@@ -11,20 +11,30 @@ import (
 
 type MonkeyValue int
 type Monkey interface {
-	Calculate(group *MonkeyGroup, depth int) MonkeyValue
+	Calculate(group *MonkeyGroup, depth int) (bool, MonkeyValue)
+	Solve(target MonkeyValue, group *MonkeyGroup, depth int) MonkeyValue
+	SolveRoot(group *MonkeyGroup) MonkeyValue
 }
 
 type MonkeyGroup map[string]Monkey
 
 type MonkeyNumber struct {
 	Value MonkeyValue
+	Human bool
+}
+
+type BinaryOp func(MonkeyValue, MonkeyValue) MonkeyValue
+
+type ExtendedBinaryOp struct {
+	Op, SolveLhs, SolveRhs BinaryOp
 }
 
 type MonkeyOp struct {
-	Operation func(MonkeyValue, MonkeyValue) MonkeyValue
-	Lhs, Rhs  string
-	cached    bool
-	cachedVal MonkeyValue
+	Operation  ExtendedBinaryOp
+	Lhs, Rhs   string
+	cached     bool
+	cachedVal  MonkeyValue
+	cachedHumn bool
 }
 
 type Parser struct {
@@ -46,24 +56,72 @@ func (group *MonkeyGroup) Get(id string) (result Monkey) {
 	return
 }
 
-func (m *MonkeyNumber) Calculate(group *MonkeyGroup, depth int) MonkeyValue {
+func (m *MonkeyNumber) Calculate(group *MonkeyGroup, depth int) (bool, MonkeyValue) {
 	debugPrint(depth, fmt.Sprintf("value: %v", m.Value))
-	return m.Value
+	return m.Human, m.Value
 }
 
-func (m *MonkeyOp) Calculate(group *MonkeyGroup, depth int) MonkeyValue {
+func (m *MonkeyOp) Calculate(group *MonkeyGroup, depth int) (bool, MonkeyValue) {
 	if m.cached {
 		debugPrint(depth, fmt.Sprintf("cached: %v", m.cachedVal))
-		return m.cachedVal
+		return m.cachedHumn, m.cachedVal
 	}
 
 	debugPrint(depth, "operation...")
-	lhs := group.Get(m.Lhs).Calculate(group, depth+1)
-	rhs := group.Get(m.Rhs).Calculate(group, depth+1)
-	m.cachedVal = m.Operation(lhs, rhs)
+	lhsHumn, lhs := group.Get(m.Lhs).Calculate(group, depth+1)
+	rhsHumn, rhs := group.Get(m.Rhs).Calculate(group, depth+1)
+	m.cachedVal = m.Operation.Op(lhs, rhs)
+	m.cachedHumn = lhsHumn || rhsHumn
 	m.cached = true
 	debugPrint(depth, fmt.Sprintf("operation got: %v", m.cachedVal))
-	return m.cachedVal
+	return m.cachedHumn, m.cachedVal
+}
+
+func (m *MonkeyNumber) Solve(target MonkeyValue, group *MonkeyGroup, depth int) MonkeyValue {
+	if m.Human {
+		return target
+	}
+	panic("Can't solve a constant")
+}
+
+func (m *MonkeyOp) Solve(target MonkeyValue, group *MonkeyGroup, depth int) MonkeyValue {
+	lhsHumn, lhs := group.Get(m.Lhs).Calculate(group, depth+1)
+	rhsHumn, rhs := group.Get(m.Rhs).Calculate(group, depth+1)
+
+	if lhsHumn && rhsHumn {
+		panic("Both subtrees can't contain human")
+	}
+
+	if lhsHumn {
+		target = m.Operation.SolveLhs(target, rhs)
+		return group.Get(m.Lhs).Solve(target, group, depth+1)
+	} else if rhsHumn {
+		target = m.Operation.SolveRhs(target, lhs)
+		return group.Get(m.Rhs).Solve(target, group, depth+1)
+	}
+
+	panic("No human in tree")
+}
+
+func (m *MonkeyNumber) SolveRoot(group *MonkeyGroup) MonkeyValue {
+	panic("Can't solve a constant")
+}
+
+func (m *MonkeyOp) SolveRoot(group *MonkeyGroup) MonkeyValue {
+	lhsHumn, lhs := group.Get(m.Lhs).Calculate(group, 0)
+	rhsHumn, rhs := group.Get(m.Rhs).Calculate(group, 0)
+
+	if lhsHumn && rhsHumn {
+		panic("Both subtrees can't contain human")
+	}
+
+	if lhsHumn {
+		return group.Get(m.Lhs).Solve(rhs, group, 0)
+	} else if rhsHumn {
+		return group.Get(m.Rhs).Solve(lhs, group, 0)
+	}
+
+	panic("No human in tree")
 }
 
 func MakeParser() (result Parser) {
@@ -80,26 +138,57 @@ func ParseInt(s string) int {
 	return result
 }
 
-func ParseOp(op string) func(MonkeyValue, MonkeyValue) MonkeyValue {
-	switch op {
+func ParseOp(symbol string) ExtendedBinaryOp {
+	var op, solveLhs, solveRhs BinaryOp
+	switch symbol {
 	case "+":
-		return func(lhs, rhs MonkeyValue) MonkeyValue {
+		op = func(lhs, rhs MonkeyValue) MonkeyValue {
 			return lhs + rhs
 		}
+		solveLhs = func(rslt, rhs MonkeyValue) MonkeyValue {
+			return rslt - rhs
+		}
+		solveRhs = func(rslt, lhs MonkeyValue) MonkeyValue {
+			return rslt - lhs
+		}
 	case "-":
-		return func(lhs, rhs MonkeyValue) MonkeyValue {
+		op = func(lhs, rhs MonkeyValue) MonkeyValue {
 			return lhs - rhs
 		}
+		solveLhs = func(rslt, rhs MonkeyValue) MonkeyValue {
+			return rslt + rhs
+		}
+		solveRhs = func(rslt, lhs MonkeyValue) MonkeyValue {
+			return lhs - rslt
+		}
 	case "*":
-		return func(lhs, rhs MonkeyValue) MonkeyValue {
+		op = func(lhs, rhs MonkeyValue) MonkeyValue {
 			return lhs * rhs
 		}
+		solveLhs = func(rslt, rhs MonkeyValue) MonkeyValue {
+			return rslt / rhs
+		}
+		solveRhs = func(rslt, lhs MonkeyValue) MonkeyValue {
+			return rslt / lhs
+		}
 	case "/":
-		return func(lhs, rhs MonkeyValue) MonkeyValue {
+		op = func(lhs, rhs MonkeyValue) MonkeyValue {
 			return lhs / rhs
 		}
+		solveLhs = func(rslt, rhs MonkeyValue) MonkeyValue {
+			return rslt * rhs
+		}
+		solveRhs = func(rslt, lhs MonkeyValue) MonkeyValue {
+			return lhs / rslt
+		}
+	default:
+		panic("Invalid operation")
 	}
-	panic("Invalid operation")
+	return ExtendedBinaryOp{
+		Op:       op,
+		SolveLhs: solveLhs,
+		SolveRhs: solveRhs,
+	}
 }
 
 func (parser *Parser) ParseLine(line string) (id string, monkey Monkey) {
@@ -107,7 +196,8 @@ func (parser *Parser) ParseLine(line string) (id string, monkey Monkey) {
 	if len(matches) == 3 {
 		id = matches[1]
 		num := MonkeyValue(ParseInt(matches[2]))
-		monkey = &MonkeyNumber{Value: num}
+		human := id == "humn"
+		monkey = &MonkeyNumber{Value: num, Human: human}
 		return
 	}
 
@@ -119,6 +209,9 @@ func (parser *Parser) ParseLine(line string) (id string, monkey Monkey) {
 			Rhs:       matches[4],
 			Operation: ParseOp(matches[3]),
 		}
+		if id == "humn" {
+			panic("humn must be a number")
+		}
 		return
 	}
 
@@ -126,6 +219,11 @@ func (parser *Parser) ParseLine(line string) (id string, monkey Monkey) {
 }
 
 func main() {
+	mode2 := false
+	if (len(os.Args) > 1) && (os.Args[1] == "2") {
+		mode2 = true
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	parser := MakeParser()
 	group := MonkeyGroup{}
@@ -138,6 +236,12 @@ func main() {
 		id, monkey := parser.ParseLine(line)
 		group[id] = monkey
 	}
-	rootVal := group.Get("root").Calculate(&group, 0)
-	fmt.Println(rootVal)
+
+	if !mode2 {
+		_, rootVal := group.Get("root").Calculate(&group, 0)
+		fmt.Println(rootVal)
+	} else {
+		x := group.Get("root").SolveRoot(&group)
+		fmt.Println(x)
+	}
 }
